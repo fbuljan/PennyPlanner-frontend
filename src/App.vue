dc1d0e93ec9bfa4aa1b12ee3a902a5d243ed21b7
Filip Buljan <filipbuljan2002@gmail.com>
2024-06-08 22:39:42 +0200

    Add toolbar on landing page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <LandingPage/>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script
+>import LandingPage from './components/LandingPage.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    LandingPage,
   },
 
   data: () => ({
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <v-toolbar
+        color="primary"
+        dark
+        prominent
+      >
+  
+        <v-toolbar-title>PennyPlanner</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn>
+          Log in
+        </v-btn>
+        
+        <v-btn>
+          Sign up
+        </v-btn>
+      </v-toolbar>
+    </div>
+    <div>
+        Tekst
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'LandingPage'
+}
+</script>
